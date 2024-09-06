7c16800b662875acd719d484b5ed0da7464136b4
Remco Stoeten <remcostoeten@hotmail.com>
2024-09-06 02:21:58 +0200

    Add ui index and scripts.

---
+++ src/components/ui/index.ts
@@ -0,0 +1,16 @@
+export * from './alert';
+export * from './avatar';
+export * from './button';
+export * from './card';
+export * from './checkbox';
+export * from './dropdown-menu';
+export * from './form';
+export * from './input';
+export * from './label';
+export * from './Skeleton';
+export * from './textarea';
+export * from './toast';
+export * from './toaster';
+export * from './tooltip';
+export * from './use-toast';
+
